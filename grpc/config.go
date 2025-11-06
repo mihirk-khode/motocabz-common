2c06f86504ae9c7fc503dabcfc14810882bb04f1
mihirk-khode <mihirk@zenithive.com>
2025-11-06 13:03:40 +0530

    host

--- grpc/config.go
+++ grpc/config.go
@@ -14,22 +14,22 @@ var Services = map[string]ServiceConfig{
 	},
 	"trip-service": {
 		Name: "trip-service",
-		Host: "",
+		Host: "k8s-motocabz-tripserv-b73ea6daa4-1957740951.eu-north-1.elb.amazonaws.com",
 		Port: "50051", // Trip service gRPC port (from Kubernetes service)
 	},
 	"identity-service": {
 		Name: "identity-service",
-		Host: "",
+		Host: "k8s-motocabz-identity-a1bc81e43d-1675016665.eu-north-1.elb.amazonaws.com",
 		Port: "50057", // Identity service gRPC port (from Kubernetes service)
 	},
 	"driver-service": {
 		Name: "driver-service",
-		Host: "",
+		Host: "k8s-motocabz-driverse-ffd6064118-2130482738.eu-north-1.elb.amazonaws.com",
 		Port: "50052", // Driver service gRPC port (from Kubernetes service)
 	},
 	"rider-service": {
 		Name: "rider-service",
-		Host: "",
+		Host: "k8s-motocabz-riderser-0ca9d74497-1897693141.eu-north-1.elb.amazonaws.com",
 		Port: "50053", // Rider service gRPC port (from Kubernetes service)
 	},
 }
