3cd2091bffd878c8dddd8952689692575841fc5e
mihirk-khode <mihirk@zenithive.com>
2025-11-06 16:24:00 +0530

    changes

--- grpc/config.go
+++ grpc/config.go
@@ -2,35 +2,29 @@ package grpc
 
 type ServiceConfig struct {
 	Name string
-	Host string
 	Port string
 }
 
 var Services = map[string]ServiceConfig{
 	"payment-service": {
 		Name: "payment-service",
-		Host: "",
-		Port: "50055", // Payment service gRPC port (from Kubernetes service)
+		Port: "50006", // Payment service gRPC port (from	 Kubernetes service)
 	},
 	"trip-service": {
 		Name: "trip-service",
-		Host: "k8s-motocabz-tripserv-b73ea6daa4-2014578782.eu-north-1.elb.amazonaws.com",
-		Port: "50051", // Trip service gRPC port (from Kubernetes service)
+		Port: "50001", // Trip service gRPC port (from Kubernetes service)
 	},
 	"identity-service": {
 		Name: "identity-service",
-		Host: "k8s-motocabz-identity-a1bc81e43d-816416611.eu-north-1.elb.amazonaws.com",
-		Port: "50057", // Identity service gRPC port (from Kubernetes service)
+		Port: "50003", // Identity service gRPC port (from Kubernetes service)
 	},
 	"driver-service": {
 		Name: "driver-service",
-		Host: "k8s-motocabz-driverse-ffd6064118-284108539.eu-north-1.elb.amazonaws.com",
-		Port: "50052", // Driver service Ingress port (ALB)
+		Port: "50004", // Driver service Ingress port (ALB)
 	},
 	"rider-service": {
 		Name: "rider-service",
-		Host: "k8s-motocabz-riderser-0ca9d74497-328343501.eu-north-1.elb.amazonaws.com",
-		Port: "50053", // Rider service gRPC port (from Kubernetes service)
+		Port: "50005", // Rider service gRPC port (from Kubernetes service)
 	},
 }
 
