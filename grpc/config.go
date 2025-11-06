0f707fb9ab1bd34de464c87afaaef8e8f187dded
mihirk-khode <mihirk@zenithive.com>
2025-11-06 15:23:42 +0530

    correct host

--- grpc/config.go
+++ grpc/config.go
@@ -14,22 +14,22 @@ var Services = map[string]ServiceConfig{
 	},
 	"trip-service": {
 		Name: "trip-service",
-		Host: "k8s-motocabz-tripserv-b73ea6daa4-1957740951.eu-north-1.elb.amazonaws.com",
+		Host: "k8s-motocabz-tripserv-b73ea6daa4-2014578782.eu-north-1.elb.amazonaws.com",
 		Port: "50051", // Trip service gRPC port (from Kubernetes service)
 	},
 	"identity-service": {
 		Name: "identity-service",
-		Host: "k8s-motocabz-identity-a1bc81e43d-1675016665.eu-north-1.elb.amazonaws.com",
+		Host: "k8s-motocabz-identity-a1bc81e43d-816416611.eu-north-1.elb.amazonaws.com",
 		Port: "50057", // Identity service gRPC port (from Kubernetes service)
 	},
 	"driver-service": {
 		Name: "driver-service",
-		Host: "k8s-motocabz-driverse-ffd6064118-2130482738.eu-north-1.elb.amazonaws.com",
-		Port: "50052", // Driver service gRPC port (from Kubernetes service)
+		Host: "k8s-motocabz-driverse-ffd6064118-284108539.eu-north-1.elb.amazonaws.com",
+		Port: "50052", // Driver service Ingress port (ALB)
 	},
 	"rider-service": {
 		Name: "rider-service",
-		Host: "k8s-motocabz-riderser-0ca9d74497-1897693141.eu-north-1.elb.amazonaws.com",
+		Host: "k8s-motocabz-riderser-0ca9d74497-328343501.eu-north-1.elb.amazonaws.com",
 		Port: "50053", // Rider service gRPC port (from Kubernetes service)
 	},
 }
